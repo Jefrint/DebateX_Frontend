8da70e6ee6e44b3716567c6d39c7e148060bef16
5732 - Jefrin Thomas Mathew <5732jtm@manorama.net>
2025-10-03 11:26:21 +0530

    Upcoming and Intersted Artcle component added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import Int_Article from '../components/Int_Article'
+import Up_Debates from '../components/Up_Debates'
 
 const Home = () => {
   return (
+
+    <>
+    
     <div>Home</div>
+<Int_Article/>
+<Up_Debates/>
+    </>
   )
 }
 
